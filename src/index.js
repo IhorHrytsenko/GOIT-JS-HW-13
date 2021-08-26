e4963118c04a8044896f580fbea74955e8cc9c46
IhorHrytsenko <gisyshka@gmail.com>
2021-08-26 16:45:59 +0300

    galllery

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
+import debounsed from 'lodash.debounce';
+import cards from "./templates/card.hbs";
+
+
 const API_KEY = '23099415-b292849e49f5632c41c65f5ef';
 const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}`;
 
-const test_url = 'https://pixabay.com/api/?key=23099415-b292849e49f5632c41c65f5ef&q=yellow+flowers';
+// const test_url = 'https://pixabay.com/api/?key=23099415-b292849e49f5632c41c65f5ef&q=yellow+flowers';
+const input_text = document.querySelector("#search_img");
+const gallery = document.querySelector(".gallery");
 
-fetch(`${BASE_URL}&q=yellow+flowers`)
+let pageNumber = 1;
+
+function fetchFind () {
+
+pageNumber = 1;
+
+fetch(`${BASE_URL}&q=${input_text.textContent}&page=${pageNumber}&per_page=5`)
 .then(response =>{return response.json()})
-.then(data => console.log(data))
+.then(data => {data.hits.map(elem => createGallery(elem));})
 .catch(error => console.log(error));
+}
 
+
+const createGallery = (elem) => {
+    gallery.insertAdjacentHTML("afterbegin", cards(elem));
+};
+
+
+input_text.addEventListener ("input", debounsed(fetchFind, 500));
